95920e5e19779c4e44ba4bcaf95cb2b481fbf98b
Miguel Pinto <fc54422@alunos.fc.ul.pt>
2022-02-08 10:50:37 +0000

    'Chapter5'

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+import datetime
+from email.policy import default
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Category(models.Model):
+    name = models.CharField(max_length = 128, unique = True)
+    #Each model's name must be unique
+    views = models.IntegerField(default = 0)
+    likes = models.IntegerField(default = 0)
+    
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    #foreign key is used to create one-to-many relatioships
+    title = models.CharField(max_length = 128)
+    url = models.URLField()
+    views = models.IntegerField(default = 0)
+
+    def str(self):
+        return self.title
+
+class Question(models.Model):
+    question_text = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+    
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.choice_text
